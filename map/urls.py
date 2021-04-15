5e12ecb90cd8a6bb6f5b4746b786f6432e200589
vamsi <75676297+beingOptimusP@users.noreply.github.com>
2021-04-15 13:25:24 +0530

    a

--- map/urls.py
+++ map/urls.py
@@ -17,6 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from main import views
 
+from django.conf import settings
+
+from django.conf.urls import url
+from django.views.static import serve
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
@@ -24,4 +30,6 @@ urlpatterns = [
     path('/dashboard', views.dashboard, name='dash'),
     path('/logout', views.logoutuser, name='logout'),
     path('/login',views.loginuser,name='login'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
